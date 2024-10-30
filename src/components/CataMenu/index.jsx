ad77f5d6d5bc39a613d29a61333ec1b5504aa93e
Naty Jimenez <Naty@codersfree.com>
2024-10-29 20:29:27 -0600

    acompletacion de imagenes

--- src/components/CataMenu/index.jsx
+++ src/components/CataMenu/index.jsx
@@ -6,12 +6,102 @@ const peliculas = [
     // Array de películas con información de cada una
     {
         id: 1,
-        titulo: 'Título de la Película 1',
-        descripcion: 'Descripción corta de la película 1.',
-        imagen: 'https://via.placeholder.com/170x250',
+        titulo: 'TOMB RAIDEN',
+        descripcion: 'La secuencia de aventuras y acción que hace que Lara Croft pase de ser una joven inexperta a una dura superviviente ha sido renovada para las consolas de nueva generación',
+        imagen: 'https://www.neo2.com/wp-content/uploads/2023/10/10-peli%CC%81culas-de-accio%CC%81n-protagonizadas-por-mujeres-Tomb_Raider-894080826-large.jpg',
         genero: 'Acción',
         año: 2023,
     },
+
+    {
+        id: 2,
+        titulo: 'SCHINDERLIST',
+        descripcion: 'En Cracovia, durante la Segunda Guerra Mundial, las tropas alemanas de ocupación han forzado a los judíos polacos a vivir...',
+        imagen: 'https://i.pinimg.com/564x/ac/14/82/ac14826ba5219e1cf7eb5c0f2777a1f3.jpg',
+        genero: 'Drama',
+        año: 2023,
+    },
+
+    {
+        id: 3,
+        titulo: 'TITANIC',
+        descripcion: 'el mayor barco de pasajeros del mundo al finalizar su construcción, naufragó en las aguas del océano Atlántico durante la noche del 14 y la madrugada del 15 de abril de 1912......',
+        imagen: 'https://rufinoweb.com.ar/wp-content/uploads/2023/02/WhatsApp-Image-2023-02-10-at-13.29.59.jpeg',
+        genero: 'Drama',
+        año: 2023,
+    },
+
+
+    {
+        id: 4,
+        titulo: 'THE NOTEBOOK',
+        descripcion: 'En una residencia de ancianos, un hombre mayor (James Garner) lee a una mujer (Gena Rowlands) una historia de amor escrita en su viejo libro de',
+        imagen: 'https://images.ecestaticos.com/ZJcV5RosehEDZCcPg9lyLDRZLIM=/0x0:1014x1497/1440x1920/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F2ac%2F4c5%2Fb9c%2F2ac4c5b9c84d99cf9bf3e6b6f61af93a.jpg',
+        genero: 'Acción',
+        año: 2023,
+    },
+
+
+    {
+        id: 5,
+        titulo: 'YES DAY',
+        descripcion: 'YES DAY....',
+        imagen: 'https://content.clara.es/medio/2022/10/13/el-dia-del-si-2021_9de80a70_1000x1500.jpg',
+        genero: 'comedia',
+        año: 2023,
+    },
+    
+    {
+        id: 6,
+        titulo: 'CRANK 2',
+        descripcion: 'un asesino a sueldo se pasa 24 horas sin descanso, peleando, matando y manteniendo el flujo de adrenalina en su …',
+        imagen: 'https://espectadores.net/wp-content/uploads/2012/08/las-mejores-peliculas-de-accion-crank-alto-voltaje-600x846.jpg',
+        genero: 'Acción',
+        año: 2023,
+    },
+    {
+        id: 7,
+        titulo: 'NO SE ACEPTAN DEVOLUCIONES',
+        descripcion: 'Valentín, un tipo mujeriego, soltero y egoísta, de repente, tiene que hacerse cargo de una hija a la que no conocía, fruto de una aventura ocasional.',
+        imagen: 'https://i.pinimg.com/originals/18/4e/06/184e06d56505ec273542b543278ea2bb.jpg',
+        genero: 'comedia',
+        año: 2023,
+    },
+    {
+        id: 8,
+        titulo: 'BAJO EL MISMO TECHO',
+        descripcion: 'Una Historia Real de Amor, Pérdida y Familia Bajo el mismo techo (conocida como life as we know it en inglés) es una película que nos cuenta la historia de Holly y Eric, dos personas que se detestan',
+        imagen: 'https://i.pinimg.com/originals/66/3f/b4/663fb438758ad17e93e34bbedd310fcf.jpg',
+        genero: 'comedia',
+        año: 2023,
+    },
+    {
+        id: 9,
+        titulo: '¿Y DONDE ESTAN LAS RUBIAS?',
+        descripcion: 'Dos ambiciosos -pero con poca fortuna- agentes del FBI (Shawn y Marlon Wayans) se hacen pasar por mujeres, novatas en la alta sociedad, en el exclusivo complejo Hamptons para investigar un círculo de secuestros.',
+        imagen: 'https://img.buzzfeed.com/buzzfeed-static/static/2022-12/20/23/asset/03732287ed65/sub-buzz-4466-1671580437-10.png',
+        genero: 'Acción',
+        año: 2023,
+    },
+    {
+        id: 10,
+        titulo: 'LITTLE MAN',
+        descripcion: 'es una película de comedia estadounidense dirigida por Keenen Ivory Wayans. Está protagonizada por Marlon Wayans, Shawn Wayans, Kerry Washington y Tracy Morgan.',
+        imagen: 'https://media.s-bol.com/qxRDBXAnA247/834x1200.jpg',
+        genero: 'comedia',
+        año: 2023,
+    },
+    {
+        id: 11,
+        titulo: '¿QUIEN MANDA?',
+        descripcion: 'Alex es un tipo relajado, sofisticado y un gran conquistador, que se conoce todos los trucos para conseguirse a cualquier mujer. ',
+        imagen: 'https://2.bp.blogspot.com/-jPz_m-DEBr8/UhN-WbJcIjI/AAAAAAAABSg/PQkHKLh_IMs/s1600/Castillo-Quien+manda.jpg',
+        genero: 'Acción',
+        año: 2023,
+    },
+
+    
+
     // Agrega más películas aquí
 ];
 
